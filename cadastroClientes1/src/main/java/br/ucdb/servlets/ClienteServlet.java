cafe6aeb9cf78811dcb7dbd5ecaf11b8bb8d6c84
fabicristine <fabi.cristine@hotmail.com>
2015-03-04 01:36:50 -0300

    primeiro commit cadastro de clientes

---
+++ cadastroClientes1/src/main/java/br/ucdb/servlets/ClienteServlet.java
@@ -0,0 +1,33 @@
+package br.ucdb.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.ucdb.util.Cliente;
+
+public class ClienteServlet extends HttpServlet {
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		String nome = req.getParameter("name");
+		String rg = req.getParameter("rg");
+		String cpf = req.getParameter("cpf");
+		String email = req.getParameter("email"); 
+		String sexo = req.getParameter("sexo");
+		String id= req.getParameter("id");
+		
+		int id1 = Integer.parseInt(id);
+		
+		Cliente c = new Cliente();
+		c.cadastrar(nome, rg, cpf, email, sexo, id);
+		
+		req.setAttribute("nome", c.getNome());
+		
+		
+	}
+	
+}
---
+++ cadastroClientes1/src/main/java/br/ucdb/util/Cliente.java
@@ -0,0 +1,51 @@
+package br.ucdb.util;
+
+public class Cliente {
+	private int id;
+	private String nome, sexo, rg, cpf, email;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	public String getRg() {
+		return rg;
+	}
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public void cadastrar(String nome2, String rg2, String cpf2, String email2,
+			String sexo2, String id2) {
+	setNome(nome2);
+	setCpf(cpf2);
+	setEmail(email2);
+		
+	}
+	
+}
